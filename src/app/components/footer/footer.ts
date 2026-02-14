1993f8ea707574a56910bce940744a60144db8af
oRonold <or040918@gmail.com>
2026-02-14 12:21:35 -0300

    Create footer section

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+
+}
