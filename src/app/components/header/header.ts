93689c85ae072f6755b79e21267c0ac17378fef6
oRonold <or040918@gmail.com>
2026-02-14 12:20:07 -0300

    Create Header

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+
+}
